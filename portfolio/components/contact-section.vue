53fdc6814d2923aa7124313ab2bcc0469fe692dc
Kaylee Williams <65376239+KayleeWilliams@users.noreply.github.com>
2022-06-02 14:38:03 +0100

    Added external links to email and linkedin to contact section.

--- portfolio/components/contact-section.vue
+++ portfolio/components/contact-section.vue
@@ -3,8 +3,13 @@
         <p class="text-4xl font-bold"> Contact Me! </p>
         <p> Feel free to get in contact with me by Email or on Linkedin. </p>
         <div class="flex flex-row gap-8">
-            <MdiEmail class="w-12 h-12 text-button hover:text-highlight" />
-            <MdiLinkedin class="w-12 h-12 text-button hover:text-highlight" />
+            <a href="mailto:kaylee@kayleewilliams.dev" target="_blank">
+                <MdiEmail class="w-12 h-12 text-button hover:text-highlight" />
+            </a>
+
+             <a href="https://www.linkedin.com/in/kaylee-williams-1559251bb" target="_blank">
+                <MdiLinkedin class="w-12 h-12 text-button hover:text-highlight" />
+            </a>
         </div>
     </div>
 
