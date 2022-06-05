91fefcc29797e6808aeda1c6dd29d5d76a67b46f
Kaylee Williams <65376239+KayleeWilliams@users.noreply.github.com>
2022-06-05 23:17:43 +0100

    Added Favicon and set page title & description.

--- portfolio/nuxt.config.ts
+++ portfolio/nuxt.config.ts
@@ -3,11 +3,16 @@ import Icons from 'unplugin-icons/vite'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    meta: {
+      title: "Kaylee Williams Portfolio",
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+      meta:[{ name: 'description', content: "Hi, I'm Kaylee Williams, a Software Engineer. Come check out some of the projects I've worked on!" }],
+    },
+
     modules: ['@nuxtjs/tailwindcss'],
     vite: {
     plugins: [
         Icons({
-        // the feature below is experimental ⬇️
         autoInstall: true
       })
     ]
